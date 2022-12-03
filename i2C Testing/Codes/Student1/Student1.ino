931dfc708ee8cb40c62a16afce2c88447104a8f7
NICHOLAS1076 <nicholasniles0@gmail.com>
2022-12-02 22:42:39 -0500

    v2.1 - Working i2c for arduino + kiCad design

---
+++ i2C Testing/Codes/Student1/Student1.ino
@@ -0,0 +1,36 @@
+#include <Wire.h>
+
+int x = 0;
+
+void setup()
+{
+  pinMode(13, OUTPUT);
+  
+  Wire.begin(1);
+  Wire.onReceive(readData);
+  
+  //PORTB_PIN0CTRL |= PORT_PULLUPEN_bm;
+  //PORTB_PIN1CTRL |= PORT_PULLUPEN_bm;
+}
+
+void loop()
+{
+  if (x == 1)
+  {
+    x = 0;
+    digitalWrite(10, HIGH);
+    delay(100);
+    digitalWrite(10, LOW);
+  }
+  else{
+    delay(100);
+  }
+}
+
+void readData()
+{
+  while (0 < Wire.available())
+  {
+    x = Wire.read();
+  }
+}
