cb5442ab14943915686749c3b8dc6e9e535b3195
NICHOLAS1076 <nicholasniles0@gmail.com>
2022-12-05 16:54:06 -0500

    v2.2 - WORKING I2C

--- i2C Testing/Codes/Master/Master.ino
+++ i2C Testing/Codes/Master/Master.ino
@@ -5,19 +5,20 @@ void setup()
 {
   pinMode(10, OUTPUT);
   
-  Wire.begin(0);
-  //PORTB_PIN0CTRL |= PORT_PULLUPEN_bm ;
-  //PORTB_PIN1CTRL |= PORT_PULLUPEN_bm;
+  Wire.begin();
+  
+  PORTB_PIN0CTRL |= PORT_PULLUPEN_bm ;
+  PORTB_PIN1CTRL |= PORT_PULLUPEN_bm;
 }
 
 void loop()
 {
-  delay(3000);
+  delay(1500);
   PingStudentOne();
   digitalWrite(10, HIGH);
   delay(200);
   digitalWrite(10, LOW);
-  delay(3000);
+  delay(1500);
   PingStudentTwo();
   digitalWrite(10, HIGH);
   delay(50);
@@ -34,14 +35,14 @@ void PingStudentOne()
 {
   Wire.beginTransmission(1);
   Wire.write(1);
-  delay(1000);
   Wire.endTransmission();
+  delay(1000);
 }
 
 void PingStudentTwo()
 {
   Wire.beginTransmission(2);
   Wire.write(1);
-  delay(1000);
   Wire.endTransmission();
+  delay(1000);
 }
