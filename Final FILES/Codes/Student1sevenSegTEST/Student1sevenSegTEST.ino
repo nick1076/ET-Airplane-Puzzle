3c16c1d8e644943ea5da542e674cc0d9446254eb
NICHOLAS1076 <nicholasniles0@gmail.com>
2022-12-06 19:29:07 -0500

    v2.6 - LCD 2.0

--- Final FILES/Codes/Student1sevenSegTEST/Student1sevenSegTEST.ino
+++ Final FILES/Codes/Student1sevenSegTEST/Student1sevenSegTEST.ino
@@ -1,24 +1,23 @@
 #include <Wire.h>
-#include <Adafruit_GFX.h> //***
-#include "Adafruit_LEDBackpack.h" //***
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
 
 int x = 0;
-
-Adafruit_7segment matrix = Adafruit_7segment(); //***
+Adafruit_7segment matrix = Adafruit_7segment();
 
 void setup()
 {
-  matrix.begin(0x70); //***
-  
-  PORTB_PIN0CTRL |= PORT_PULLUPEN_bm;
-  PORTB_PIN1CTRL |= PORT_PULLUPEN_bm;
+  matrix.begin(0x70);
   
   pinMode(10, OUTPUT);
   
   Wire.begin(1);
   Wire.onReceive(readData);
-
-  DisplayCode(0, 0, 0, 0);
+  
+  DisplayCode(0, 1, 0, 0);
+  
+  PORTB_PIN0CTRL |= PORT_PULLUPEN_bm;
+  PORTB_PIN1CTRL |= PORT_PULLUPEN_bm;
 }
 
 void loop()
@@ -27,18 +26,10 @@ void loop()
   {
     x = 0;
     digitalWrite(10, HIGH);
-    DisplayCode(1, 2, 3, 4); //***
-    delay(250);
-    digitalWrite(10, LOW);
-    DisplayCode(0, 0, 0, 0); //***
-  }
-  if (x == 2)
-  {
-    x = 0;
-    digitalWrite(10, HIGH);
+    DisplayCode(1, 2, 3, 4);
     delay(100);
     digitalWrite(10, LOW);
-    DisplayCode(0, 0, 0, 0); //***
+    DisplayCode(0, 2, 0, 0);
   }
   else{
     delay(100);
@@ -53,7 +44,6 @@ void readData()
   }
 }
 
- //***Following Method
 void DisplayCode(int num1, int num2, int num3, int num4){
     matrix.writeDigitNum(0, num1, false);
     matrix.writeDigitNum(1, num2, false);
